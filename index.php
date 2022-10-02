33594040dda235add79931c666697d97813679a1
Ed <dev@ed.palych>
2022-10-03 00:11:09 +0300

    fix

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 ini_set('memory_limit', '-1');
 set_time_limit(0);
 
+// chdir(__DIR__);
+// exec('git pull 2>&1', $output);
+// echo implode("\n", $output);
+
 $app = JFactory::getApplication();
 defined("DBDRIVER")or define("DBDRIVER","mysql");
 defined("DBHOST")or define("DBHOST", $app->getCfg('host', ''));
