4f20cb4b38983427fbb303c28fa48c72882b0481
wegas <wegas22@yandex.ru>
2022-10-18 00:19:25 +0300

    soliderComponent

---
+++ app/models/ComponentSolidresReservation.php
@@ -0,0 +1,18 @@
+<?
+
+namespace Wegas\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class ComponentSolidresReservation extends Model {
+
+	protected $table = 'sr_reservations';
+
+	protected $guarded = [];
+	public $timestamps = false;
+
+	protected $primaryKey = 'id';
+
+}
+
+
+
