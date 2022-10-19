7156426fc3fd650663681ad86299a57fe56e0085
wegas <wegas22@yandex.ru>
2022-10-19 14:25:37 +0300

    solidercomponent

--- app/models/ComponentSolidresReservation.php
+++ app/models/ComponentSolidresReservation.php
@@ -12,6 +12,9 @@ class ComponentSolidresReservation extends Model {
 
 	protected $primaryKey = 'id';
 
+	public function extra_xref() {
+		return $this->belongsToMany(ComponentSolidresRoom::class, 'sr_reservation_room_extra_xref', 'reservation_id', 'room_id')->withPivot('extra_name', 'extra_quantity', 'extra_price', 'room_label');
+	}
 }
 
 
---
+++ app/models/ComponentSolidresRoom.php
@@ -0,0 +1,18 @@
+<?
+
+namespace Wegas\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class ComponentSolidresRoom extends Model {
+
+	protected $table = 'sr_rooms';
+
+	protected $guarded = [];
+	public $timestamps = false;
+
+	protected $primaryKey = 'id';
+
+}
+
+
+
